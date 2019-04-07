42ce9ab856630d5d0aabcd8cc54d942d257909b2
jrma94 <rmqz94@Hotmail.com>
2019-04-07 00:23:21 -0500

    Add files via upload
    As it turns out, input elements are not allowed to have children nodes, so I had to fix all of that

--- js/validate.js
+++ js/validate.js
@@ -1,6 +1,3 @@
-//DOESN'T WORK JUST YET
-//not entirely sure why it doesn't select the inputs, I thought it would
-//might have to try and make the form table cells like in PW3
 $(document).ready(function() 
 {
 	//declare all needed spans and hide all of them
@@ -10,40 +7,41 @@ $(document).ready(function()
 	//since the first span of each section needs to display different info text
 	//they have to be declared separately, and then give all of them a class
 	
-	//every single span is appended to each form row, i.e., they're a new column/data cell
-	$("form").find(":input:first").append("<span>Enter your first name</span>");
-	$("form").find(":input:nth-of-type(2)").append("<span>Enter your last name</span>");
-	$("form").find(":input:nth-of-type(3)").append("<span>Enter your street address</span>");
-	$("form").find(":input:nth-of-type(4)").append("<span>Enter your city</span>");
-	$("form").find(":input:nth-of-type(5)").append("<span>Enter your state</span>");
-	$("form").find(":input:nth-of-type(6)").append("<span>Enter your phone number</span>");
-	$("form").find(":input:nth-of-type(7)").append("<span>Enter your email</span>");
-	$("form").find(":input:nth-of-type(8)").append("<span>Enter a password, minimum 6 characters</span>");
-	$("form").find(":input").find("span:first-of-type").addClass("info");
+	//every single span is appended to each table row, i.e., they're a new column/data cell
+	$("table").find("tr:first").append("<span>Enter your first name</span>");
+	$("table").find("tr:nth-child(2)").append("<span>Enter your last name</span>");
+	$("table").find("tr:nth-child(3)").append("<span>Enter your street address</span>");
+	$("table").find("tr:nth-child(4)").append("<span>Enter your city</span>");
+	$("table").find("tr:nth-child(5)").append("<span>Enter your state</span>");
+	$("table").find("tr:nth-child(6)").append("<span>Enter your phone number</span>");
+	$("table").find("tr:nth-child(7)").append("<span>Enter your email</span>");
+	$("table").find("tr:nth-child(8)").append("<span>Enter a password, minimum 6 characters</span>");
+	$("table").find("tr").find("span:first-of-type").addClass("info");
 	
-	//then we append the OK and Error spans and classes to all form rows
-	$("form").find(":input").append("<span>OK</span>");
-	$("form").find(":input").find("span:nth-of-type(2)").addClass("ok");
-	$("form").find(":input").append("<span>Error</span>");
-	$("form").find(":input").find("span:nth-of-type(3)").addClass("error");
+	//then we append the OK and Error spans and add classes to all table rows
+	$("table").find("tr").append("<span>OK</span>");
+	$("table").find("tr").find("span:nth-of-type(2)").addClass("ok");
+	$("table").find("tr").append("<span>Error</span>");
+	$("table").find("tr").find("span:nth-of-type(3)").addClass("error");
 	
 	//then we hide all messages
-	$("form").find(":input").find(".info").hide();
-	$("form").find(":input").find(".ok").hide();
-	$("form").find(":input").find(".error").hide();
+	$("table").find("tr").find(".info").hide();
+	$("table").find("tr").find(".ok").hide();
+	$("table").find("tr").find(".error").hide();
 	
+	//first name input
 	$("#fname").focusin(function() 
 	{
-		$("form").find(":input:first").find(".info").show();
-		$("form").find(":input:first").find(".ok").hide();
-		$("form").find(":input:first").find(".error").hide();
+		$("table").find("tr:first").find(".info").show();
+		$("table").find("tr:first").find(".ok").hide();
+		$("table").find("tr:first").find(".error").hide();
 	});
 	
 	$("#fname").focusout(function() 
 	{
 		if($("#fname").val() == "") 
 		{
-			$("form").find(":input:first").find(".info").hide();
+			$("table").find("tr:first").find(".info").hide();
 		} 
 		else if($("#fname").val() != "") 
 		{
@@ -51,60 +49,185 @@ $(document).ready(function()
 			var entry = $("#fname").val();
 			if(letters.test(entry)) //test() returns true if it matches
 			{
-				$("form").find(":input:first").find(".info").hide();
-				$("form").find(":input:first").find(".ok").show();
+				$("table").find("tr:first").find(".info").hide();
+				$("table").find("tr:first").find(".ok").show();
 			}	
 			else 
 			{
-				$("form").find(":input:first").find(".info").hide();
-				$("form").find(":input:first").find(".error").show();
-			}
-		} 
-	});
-	
-	
-	$("#password").focusin(function() 
-	{
-		$("form").find(":input:nth-of-type(2)").find(".info").show();
-		$("form").find(":input:nth-of-type(2)").find(".ok").hide();
-		$("form").find(":input:nth-of-type(2)").find(".error").hide();
-	});
-	
-	$("#password").focusout(function() 
-	{
-		if($("#password").val() == "") 
-		{
-			$("form").find(":input:nth-of-type(2)").find(".info").hide();
-		} 
-		else if($("#password").val() != "") 
-		{
-			var passlength = $("#password").val();
-			if(passlength.length >= 6)
-			{
-				$("form").find(":input:nth-of-type(2)").find(".info").hide();
-				$("form").find(":input:nth-of-type(2)").find(".ok").show();
-			}	
-			else 
-			{
-				$("form").find(":input:nth-of-type(2)").find(".info").hide();
-				$("form").find(":input:nth-of-type(2)").find(".error").show();
+				$("table").find("tr:first").find(".info").hide();
+				$("table").find("tr:first").find(".error").show();
 			}
 		} 
 	});
 
+	//last name input
+	$("#lname").focusin(function() 
+	{
+		$("table").find("tr:nth-child(2)").find(".info").show();
+		$("table").find("tr:nth-child(2)").find(".ok").hide();
+		$("table").find("tr:nth-child(2)").find(".error").hide();
+	});
+	
+	$("#lname").focusout(function() 
+	{
+		if($("#lname").val() == "") 
+		{
+			$("table").find("tr:nth-child(2)").find(".info").hide();
+		} 
+		else if($("#lname").val() != "") 
+		{
+			var letters = /^[0-9a-zA-Z]+$/;
+			var entry = $("#lname").val();
+			if(letters.test(entry)) //test() returns true if it matches
+			{
+				$("table").find("tr:nth-child(2)").find(".info").hide();
+				$("table").find("tr:nth-child(2)").find(".ok").show();
+			}	
+			else 
+			{
+				$("table").find("tr:nth-child(2)").find(".info").hide();
+				$("table").find("tr:nth-child(2)").find(".error").show();
+			}
+		} 
+	});
 
+	//address name input
+	$("#address").focusin(function() 
+	{
+		$("table").find("tr:nth-child(3)").find(".info").show();
+		$("table").find("tr:nth-child(3)").find(".ok").hide();
+		$("table").find("tr:nth-child(3)").find(".error").hide();
+	});
+	
+	$("#address").focusout(function() 
+	{
+		if($("#address").val() == "") 
+		{
+			$("table").find("tr:nth-child(3)").find(".info").hide();
+		} 
+		else if($("#address").val() != "") 
+		{
+			var letters = /^[0-9a-zA-Z]+$/;
+			var entry = $("#address").val();
+			if(letters.test(entry)) //test() returns true if it matches
+			{
+				$("table").find("tr:nth-child(3)").find(".info").hide();
+				$("table").find("tr:nth-child(3)").find(".ok").show();
+			}	
+			else 
+			{
+				$("table").find("tr:nth-child(3)").find(".info").hide();
+				$("table").find("tr:nth-child(3)").find(".error").show();
+			}
+		} 
+	});
+
+	//city name input
+	$("#city").focusin(function() 
+	{
+		$("table").find("tr:nth-child(4)").find(".info").show();
+		$("table").find("tr:nth-child(4)").find(".ok").hide();
+		$("table").find("tr:nth-child(4)").find(".error").hide();
+	});
+	
+	$("#city").focusout(function() 
+	{
+		if($("#city").val() == "") 
+		{
+			$("table").find("tr:nth-child(4)").find(".info").hide();
+		} 
+		else if($("#city").val() != "") 
+		{
+			var letters = /^[0-9a-zA-Z]+$/;
+			var entry = $("#city").val();
+			if(letters.test(entry)) //test() returns true if it matches
+			{
+				$("table").find("tr:nth-child(4)").find(".info").hide();
+				$("table").find("tr:nth-child(4)").find(".ok").show();
+			}	
+			else 
+			{
+				$("table").find("tr:nth-child(4)").find(".info").hide();
+				$("table").find("tr:nth-child(4)").find(".error").show();
+			}
+		} 
+	});
+
+	//state name input
+	$("#state").focusin(function() 
+	{
+		$("table").find("tr:nth-child(5)").find(".info").show();
+		$("table").find("tr:nth-child(5)").find(".ok").hide();
+		$("table").find("tr:nth-child(5)").find(".error").hide();
+	});
+	
+	$("#state").focusout(function() 
+	{
+		if($("#state").val() == "") 
+		{
+			$("table").find("tr:nth-child(5)").find(".info").hide();
+		} 
+		else if($("#state").val() != "") 
+		{
+			var letters = /^[0-9a-zA-Z]+$/;
+			var entry = $("#state").val();
+			if(letters.test(entry)) //test() returns true if it matches
+			{
+				$("table").find("tr:nth-child(5)").find(".info").hide();
+				$("table").find("tr:nth-child(5)").find(".ok").show();
+			}	
+			else 
+			{
+				$("table").find("tr:nth-child(5)").find(".info").hide();
+				$("table").find("tr:nth-child(5)").find(".error").show();
+			}
+		} 
+	});
+
+	//phone name input
+	$("#phone").focusin(function() 
+	{
+		$("table").find("tr:nth-child(6)").find(".info").show();
+		$("table").find("tr:nth-child(6)").find(".ok").hide();
+		$("table").find("tr:nth-child(6)").find(".error").hide();
+	});
+	
+	$("#phone").focusout(function() 
+	{
+		if($("#phone").val() == "") 
+		{
+			$("table").find("tr:nth-child(6)").find(".info").hide();
+		} 
+		else if($("#phone").val() != "") 
+		{
+			var letters = /^[0-9a-zA-Z]+$/;
+			var entry = $("#phone").val();
+			if(letters.test(entry)) //test() returns true if it matches
+			{
+				$("table").find("tr:nth-child(6)").find(".info").hide();
+				$("table").find("tr:nth-child(6)").find(".ok").show();
+			}	
+			else 
+			{
+				$("table").find("tr:nth-child(6)").find(".info").hide();
+				$("table").find("tr:nth-child(6)").find(".error").show();
+			}
+		} 
+	});
+	
+	//email input
 	$("#email").focusin(function() 
 	{
-		$("form").find(":input:nth-of-type(3)").find(".info").show();
-		$("form").find(":input:nth-of-type(3)").find(".ok").hide();
-		$("form").find(":input:nth-of-type(3)").find(".error").hide();
+		$("table").find("tr:nth-child(7)").find(".info").show();
+		$("table").find("tr:nth-child(7)").find(".ok").hide();
+		$("table").find("tr:nth-child(7)").find(".error").hide();
 	});
 	
 	$("#email").focusout(function() 
 	{
 		if($("#email").val() == "") 
 		{
-			$("form").find(":input:nth-of-type(3)").find(".info").hide();
+			$("table").find("tr:nth-child(7)").find(".info").hide();
 		} 
 		else if($("#email").val() != "") 
 		{
@@ -112,13 +235,43 @@ $(document).ready(function()
 			var n = emailcontent.includes("@"); //returns true if includes
 			if(n)
 			{
-				$("form").find(":input:nth-of-type(3)").find(".info").hide();
-				$("form").find(":input:nth-of-type(3)").find(".ok").show();
+				$("table").find("tr:nth-child(7)").find(".info").hide();
+				$("table").find("tr:nth-child(7)").find(".ok").show();
 			}	
 			else 
 			{
-				$("form").find(":input:nth-of-type(3)").find(".info").hide();
-				$("form").find(":input:nth-of-type(3)").find(".error").show();
+				$("table").find("tr:nth-child(7)").find(".info").hide();
+				$("table").find("tr:nth-child(7)").find(".error").show();
+			}
+		} 
+	});
+	
+	//password input
+	$("#password").focusin(function() 
+	{
+		$("table").find("tr:nth-child(8)").find(".info").show();
+		$("table").find("tr:nth-child(8)").find(".ok").hide();
+		$("table").find("tr:nth-child(8)").find(".error").hide();
+	});
+	
+	$("#password").focusout(function() 
+	{
+		if($("#password").val() == "") 
+		{
+			$("table").find("tr:nth-child(8)").find(".info").hide();
+		} 
+		else if($("#password").val() != "") 
+		{
+			var passlength = $("#password").val();
+			if(passlength.length >= 6)
+			{
+				$("table").find("tr:nth-child(8)").find(".info").hide();
+				$("table").find("tr:nth-child(8)").find(".ok").show();
+			}	
+			else 
+			{
+				$("table").find("tr:nth-child(8)").find(".info").hide();
+				$("table").find("tr:nth-child(8)").find(".error").show();
 			}
 		} 
 	});

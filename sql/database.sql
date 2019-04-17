5979a4dfb7e2b4bda3b795af384a51dda90cc760
jrma94 <rmqz94@Hotmail.com>
2019-04-16 20:17:34 -0500

    Add files via upload
    turned the tables back into what we had before

--- sql/database.sql
+++ sql/database.sql
@@ -2,29 +2,23 @@ CREATE TABLE users(
 	user_id INTEGER NOT NULL UNIQUE AUTO_INCREMENT,
 	first_name VARCHAR(30),
 	last_name VARCHAR(30),
-	address	 VARCHAR(30),
+	address	 VARCHAR(50),
 	city VARCHAR(30),
 	state VARCHAR(2),
 	phone CHAR(14),
 	email VARCHAR(50),
+	password VARCHAR(100),
 	is_admin BOOLEAN,
 	PRIMARY KEY(user_id)
 );	
-
-CREATE TABLE passwords(
-	user_id INTEGER,
-	password VARCHAR(100),
-	PRIMARY KEY(user_id),
-	FOREIGN KEY(user_id) REFERENCES users (user_id)
-);
 
 CREATE TABLE events(
 	event_id INTEGER NOT NULL UNIQUE AUTO_INCREMENT,
 	title VARCHAR(30),
 	date_of VARCHAR(20),
-	start_time VARCHAR(5),
-	end_time VARCHAR(5),
-	address	 VARCHAR(30),
+	start_time VARCHAR(7),
+	end_time VARCHAR(7),
+	address	 VARCHAR(50),
 	city VARCHAR(30),
 	state VARCHAR(2),
 	description VARCHAR(300),
@@ -41,12 +35,4 @@ CREATE TABLE is_registered(
 	PRIMARY KEY(event_id,user_id),
 	FOREIGN KEY(event_id) REFERENCES events (event_id) ON DELETE CASCADE, --I believe this is how it needs to be written
 	FOREIGN KEY(user_id) REFERENCES users (user_id)
-);
-
-
---Don't need this but it's just there just in case
-CREATE TABLE is_admin(
-	admin_id INTEGER NOT NULL UNIQUE AUTO_INCREMENT,
-	user_id INTEGER NOT NULL UNIQUE,
-	PRIMARY KEY(admin_id) REFERENCES users (user_id)
 );

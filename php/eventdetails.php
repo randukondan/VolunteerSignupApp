46f2502e94d7f50b3305e792f03f269b96f24989
jrma94 <rmqz94@Hotmail.com>
2019-04-27 19:40:42 -0500

    Add files via upload

--- php/eventdetails.php
+++ php/eventdetails.php
@@ -13,18 +13,15 @@
 	</head>
 	<body>
 		<?php 
-			$conn = mysqli_connect("127.0.0.1", "root", "", "mysql");
+			$conn = mysqli_connect("127.0.0.1", "root", "", "volunteer");
 			if (!$conn) 
 			{
 				die("Connection failed: " . mysqli_connect_error());
 			} 
-
 			//Next step: separate events as past events and upcoming events
-			$query = "SELECT event_id, title, date_of, start_time, end_time, address, city, state, description, capacity, c_name, c_phone, c_email FROM events;";
+			$query = "SELECT event_id, title, date_of, start_time, end_time, address, city, state, description, capacity, c_name, c_phone, c_email FROM events WHERE city LIKE '%'$input'%' end_time >= NOW();";
 			$result = mysqli_query($conn,$query);
-
 			echo "<form method = \"post\" action = \"./registerevent.php\">";
-
 			while($row = mysqli_fetch_array($result)) 
 			{
 				echo "<div>";
@@ -39,10 +36,16 @@
 				echo "</div></br></br>";
 			}
 			echo "<input type=\"submit\" value=\"Register\"></form>";
-
 			mysqli_close($conn);
 		?>
-	</br>
+		</br>
+		<a href="./homeuser.php"><button type="button">Go Back</button></a>
 		<a href="./logout.php"><button type="button">Logout</button></a> 
+		<div class = "sidenav">
+			<form action="/action_page.php">
+		      <input type="text" placeholder="Search.." name="search">
+		      <button type="submit">Submit</button>
+		    </form>
+		</div>
 	</body>
 </html>
--- php/homeadmin.php
+++ php/homeadmin.php
@@ -14,7 +14,7 @@
 
 <body>
 	<?php
-		$conn = mysqli_connect("127.0.0.1", "root", "", "mysql");
+		$conn = mysqli_connect("127.0.0.1", "root", "", "volunteer");
 		if (!$conn) {die("Connection failed: " . mysqli_connect_error());} 
 		$queryname = "SELECT first_name, last_name FROM users WHERE email = \"".$_SESSION["User"]."\";";
 		$result = mysqli_query($conn,$queryname);
@@ -22,15 +22,14 @@
 		$fname = $row['first_name'];
 		$lname = $row['last_name'];
 		echo "<h3>Hi there, ".$fname." ".$lname." welcome!</h3> <br><h4>You have logged in as an Admin.</h4>";
-		$queryevents = "SELECT * from events WHERE end_time >= NOW();";
+		$queryevents = "SELECT * from events;";
 		$result = mysqli_query($conn, $queryevents);
 		echo "<h4>Current events on the system:</h4>";
-
 		echo "<form method = \"post\" action = \"./modify.php\">";
-		echo "<table style = 'text-align:center'><tr><th></th><th>Name</th></tr>";
+		echo "<table style = 'text-align:center'><tr><th></th><th>Name</th><th>Date</th></tr>";
 		while($row=mysqli_fetch_array($result))
 		{
-			echo "<tr><td>"."<input type=\"radio\" name=\"eventid\" value=\"".$row['event_id']."\">"."</td><td>".$row['title']."</td></tr>";
+			echo "<tr><td>"."<input type=\"radio\" name=\"eventid\" value=\"".$row['event_id']."\">"."</td><td>".$row['title']."</td><td>".$row['date_of']."</td></tr>";
 		}
 		echo "</table>";
 		echo "<input type=\"submit\" name=\"delete\" value=\"Delete\">&nbsp &nbsp &nbsp";

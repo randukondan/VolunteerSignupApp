5aaf2d0798b9b3a116d65ff9cf4c7ea3cbf9028f
randukondan <raja.andukondan@gmail.com>
2019-04-29 00:18:02 -0500

    Updated date displayed to use start_time

--- php/userhistory.php
+++ php/userhistory.php
@@ -29,7 +29,7 @@
 			<div class="col-sm-8" style="text-align: center"> <!-- creates big column mid -->
 			<p><b>Here are your upcoming events:</b></p>
 				<?php
-					$conn = mysqli_connect("127.0.0.1", "root", "", "volunteer");
+					$conn = mysqli_connect("127.0.0.1", "root", "", "mysql");
 					if (!$conn) 
 					{
 						die("Connection failed: " . mysqli_connect_error());
@@ -40,13 +40,13 @@
 					$result = mysqli_query($conn,$queryupcoming);
 					while($row = mysqli_fetch_array($result))
 					{
-						$queryfetch = "SELECT title, date_of, start_time FROM events WHERE event_id = \"".$row['event_id']."\";";
+						$queryfetch = "SELECT title, start_time FROM events WHERE event_id = \"".$row['event_id']."\";";
 						$result2 = mysqli_query($conn, $queryfetch);
 						$row2 = mysqli_fetch_array($result2);
 						$querytime = "SELECT CAST(start_time AS time) FROM events WHERE event_id = \"".$row['event_id']."\"; ";
 						$result3 = mysqli_query($conn, $querytime);
 						$row3 = mysqli_fetch_array($result3);
-						echo $row2['title']." on ".$row2['date_of']." at ".$row3['0']."</br>";
+						echo $row2['title']." on ".date("dS F Y", strtotime(substr($row2['start_time'], 0, 10)))." at ".$row3['0']."</br>";
 					}
 					echo "</br>";
 					echo "<p><b>Here are your past events:</b></p>";
@@ -54,10 +54,10 @@
 					$result = mysqli_query($conn,$querypast);
 					while($row = mysqli_fetch_array($result))
 					{
-						$queryfetch = "SELECT title, date_of FROM events WHERE event_id = \"".$row['event_id']."\";";
+						$queryfetch = "SELECT title, start_time FROM events WHERE event_id = \"".$row['event_id']."\";";
 						$result2 = mysqli_query($conn, $queryfetch);
 						$row2 = mysqli_fetch_array($result2);
-						echo $row2['title']." on ".$row2['date_of']."</br>";
+						echo $row2['title']." on ".date("dS F Y", strtotime(substr($row2['start_time'], 0, 10)))."</br>";
 					}
 					echo "</br>";	
 					mysqli_close($conn);	
